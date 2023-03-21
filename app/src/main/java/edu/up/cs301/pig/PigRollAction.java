90a3a7805c925d81bc0f6d8a6ec1023c4f5651e7
aaron <stoll23@up.edu>
2023-03-21 16:48:50 -0700

    added log

--- app/src/main/java/edu/up/cs301/pig/PigRollAction.java
+++ app/src/main/java/edu/up/cs301/pig/PigRollAction.java
@@ -1,13 +1,16 @@
 package edu.up.cs301.pig;
 
 import android.annotation.SuppressLint;
+import android.util.Log;
 
 import edu.up.cs301.game.GamePlayer;
 import edu.up.cs301.game.actionMsg.GameAction;
 
 
 public class PigRollAction extends GameAction {
-    public PigRollAction(GamePlayer player){super(player);
+    public PigRollAction(GamePlayer player){
+        super(player);
+        Log.d("PigRollAction", "PigRollAction constructor called");
     }
 
 }
